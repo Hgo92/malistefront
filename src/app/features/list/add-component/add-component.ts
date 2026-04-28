3bf2d2e78d0ad7d48cf57134db9717beca2643e4
Hgo92 <hugo_monier@yahoo.fr>
2026-04-28 23:23:17 +0200

    mise en place d'un dialog pour add item

--- src/app/features/list/add-component/add-component.ts
+++ src/app/features/list/add-component/add-component.ts
@@ -3,15 +3,17 @@ import { ItemService } from '../services/item';
 import { FormsModule } from '@angular/forms';
 import { Item } from '../models/item';
 import { Router } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-component',
-  imports: [FormsModule],
+  imports: [FormsModule, MatDialogModule],
   templateUrl: './add-component.html',
   styleUrl: './add-component.scss',
 })
 export class AddComponent {
   newItemName = '';
+  newItemNumber = 1;
   items: Item[] = [];
   
   constructor(private item: ItemService, private router : Router) {}
@@ -21,6 +23,7 @@ export class AddComponent {
   addItem() {
     return this.item.add(this.newItemName).subscribe(() => {
       this.itemAdded.emit();
+      this.router.navigate(['/list'])
     });
   }
 
