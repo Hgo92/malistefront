6af3cd4c5403a0291961ea19fc24c8f61683799e
Hgo92 <hugo_monier@yahoo.fr>
2026-04-28 14:04:50 +0200

    correction import inutile

--- src/app/header-component/header-component.ts
+++ src/app/header-component/header-component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-component',
-  imports: [Auth],
+  imports: [],
   templateUrl: './header-component.html',
   styleUrl: './header-component.scss',
 })
