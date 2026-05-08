1bd0a01c8d2cdc58b3e311f3ddc934b95b92dbf9
Hgo92 <hugo_monier@yahoo.fr>
2026-05-08 20:41:39 +0200

    test - changement du form register

--- src/app/features/authentification/register-component/register-component.ts
+++ src/app/features/authentification/register-component/register-component.ts
@@ -1,29 +1,62 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth } from '../services/auth';
-import { FormsModule } from '@angular/forms';
+import { form, FormField, FormRoot, minLength, required } from '@angular/forms/signals';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-register-component',
-  imports: [FormsModule],
+  imports: [FormField, FormRoot],
+  standalone: true,
   templateUrl: './register-component.html',
   styleUrl: './register-component.scss',
 })
 
+
+
 export class RegisterComponent {
-  username = '';
-  password = '';
+  registerModel = signal({
+    username: '',
+    password:'',
+  });
   
   private readonly router = inject(Router);
   private readonly auth = inject(Auth);
 
-  constructor() {}
 
-  onRegister() {this.auth.register(this.username, this.password).subscribe({
+  registerForm = form(
+    this.registerModel,
+    (schemaPath) => {
+      required(schemaPath.username);
+      minLength(schemaPath.username, 3)
+      required(schemaPath.password);
+      minLength(schemaPath.password, 8);
+    },
+    {
+      submission : {
+        action: async (field) => {
+          const { username, password } = field().value()
+          
+          try {
+            await firstValueFrom(this.auth.register(username, password));
+            return;
+          } catch (error) {
+            return { kind : 'serverError', message: "L'inscription n'a pas fonctionné"}
+          }
+        }
+      }
+    }
+  );
+
+  onRegister(username : string, password : string) {this.auth.register(username, password).subscribe({
     next: () => this.router.navigate(['/login']), 
     error : () => console.error("Erreur lors de l'inscription")
   });
-} 
+  } 
+
+  onSubmit() {
+    this.router.navigate(['/login'])
+  }
 
   onCancel() {
     this.router.navigate(['/']);
