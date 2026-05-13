27834f64c7a89c2925f3e2e22831bd47d50c1a4a
Hgo92 <hugo_monier@yahoo.fr>
2026-05-13 14:02:10 +0200

    refactor - changement syntaxe des subscribes dans list + ajout d'un sort

--- src/app/features/list/list-component/list-component.ts
+++ src/app/features/list/list-component/list-component.ts
@@ -1,7 +1,7 @@
 import { Observable, Subject } from 'rxjs';
 import { map, shareReplay, startWith, switchMap } from 'rxjs/operators';
 
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Auth } from '@authentication/services/auth';
 
@@ -11,6 +11,7 @@ import { ItemComponent } from '../item-component/item-component';
 import { Item } from '../models/item';
 import { ItemService } from '../services/item';
 import { AsyncPipe } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-list-component',
@@ -22,6 +23,7 @@ export class ListComponent implements OnInit {
   username = '';
 
   private refresh$ = new Subject<void>();
+  private readonly destroyRef = inject(DestroyRef);
   private readonly auth = inject(Auth);
   private readonly itemService = inject(ItemService);
   private readonly dialog = inject(MatDialog);
@@ -32,9 +34,13 @@ export class ListComponent implements OnInit {
     shareReplay(1),
   );
 
-  activeItems$ = this.items$.pipe(map((items) => items.filter((item) => !item.isArchived)));
+  activeItems$ = this.items$.pipe(
+    map((items) => items.filter((item) => !item.isArchived).sort((a, b) => a.id - b.id)),
+  );
 
-  archivedItems$ = this.items$.pipe(map((items) => items.filter((item) => item.isArchived)));
+  archivedItems$ = this.items$.pipe(
+    map((items) => items.filter((item) => item.isArchived).sort((a, b) => a.id - b.id)),
+  );
 
   // Au lancement, je récupère l'username pour pouvoir l'afficher
   ngOnInit() {
@@ -48,21 +54,33 @@ export class ListComponent implements OnInit {
 
   // Mes méthodes pour détacher un item d'une liste, l'attacher et le supprimer
   onDetach(id: number) {
-    this.itemService.detach(id).subscribe(() => this.loadItems());
+    this.itemService
+      .detach(id)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => this.loadItems());
   }
 
   onAttach(id: number) {
-    this.itemService.attach(id).subscribe(() => this.loadItems());
+    this.itemService
+      .attach(id)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => this.loadItems());
   }
 
   onDelete(id: number) {
-    this.itemService.delete(id).subscribe(() => this.loadItems());
+    this.itemService
+      .delete(id)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => this.loadItems());
   }
 
   // Mon Dialog/modale pour ajouter un item
   toAddComponent() {
     const dialogRef = this.dialog.open(AddComponent);
 
-    dialogRef.afterClosed().subscribe(() => this.loadItems());
+    dialogRef
+      .afterClosed()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => this.loadItems());
   }
 }
